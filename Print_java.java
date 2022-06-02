1403f93edcdc2743fa5dfa57e1924d0c2991ebb0
Hrishi12072000 <103590267+Hrishi12072000@users.noreply.github.com>
2022-06-02 16:31:55 +0530

    Add files via upload

---
+++ Print_java.java
@@ -0,0 +1,17 @@
+package thursday_lab2;
+
+public class Print_java
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("      J    A     V     V     A          ");
+		System.out.println("      J   A A     V   V     A A         ");
+		System.out.println("   J  J  AAAAA     V V     AAAAA        ");
+		System.out.println("    JJJ A     A     V     A     A       ");
+
+	}
+
+}
