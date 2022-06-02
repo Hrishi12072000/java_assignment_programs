286e5e26d78394b9d25cc1aa46d3973c29ea9b89
Hrishi12072000 <103590267+Hrishi12072000@users.noreply.github.com>
2022-06-02 17:57:37 +0530

    Add files via upload

---
+++ Ass_2june_3_table_24_50_29.java
@@ -0,0 +1,31 @@
+package Lab3;
+
+public class Ass_2june_3_table_24_50_29
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		int n=0,i,num=24,num1=50,num2=29;
+		System.out.println("Enter the number who's table you want");
+		
+		for(i=1;i<11;i++)
+		{
+				System.out.println(num+"*"+i+"="+num*i);	
+		}
+		System.out.println("");
+		
+		for(i=1;i<11;i++)
+		{
+				System.out.println(num1+"*"+i+"="+num1*i);
+		}
+		System.out.println("");
+
+		for(i=1;i<11;i++)
+		{
+				System.out.println(num2+"*"+i+"="+num2*i);
+		}
+
+	}
+
+}
